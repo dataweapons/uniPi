08da7f5f13c2f309b3cbb7504431d9a26da3f48f
dataWeapons <Dataweapons@gmail.com>
2016-02-07 16:04:30 -1000

    postInstall.sh

---
+++ postInstall.sh
@@ -0,0 +1,12 @@
+sudo sed -i 's@^set_java_home$@#set_java_home\n\n# Use Oracle Java 8 JVM instead.\nJAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt@' /usr/lib/unifi/bin/unifi.init
+
+sudo cp /lib/systemd/system/unifi.service /etc/systemd/system/
+sudo sed -i '/^\[Service\]$/a Environment=JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm-vfp-hflt' /etc/systemd/system/unifi.service
+
+sudo sed -i 's@-Xmx1024M@-Xmx384M@' /usr/lib/unifi/bin/unifi.init
+sudo sed -i 's@-Xmx1024M@-Xmx768M@' /usr/lib/unifi/bin/unifi.init
+
+sudo chown -R unifi:unifi /var/lib/unifi /var/log/unifi /var/run/unifi /usr/lib/unifi/work
+sudo sed -i '/^\[Service\]$/a User=unifi' /etc/systemd/system/unifi.service
+
+sudo echo 'ENABLE_MONGODB=no' | sudo tee -a /etc/mongodb.conf > /dev/null
